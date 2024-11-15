5abe7d86fa7c36fbd9d7cfdb7f8520a1f5215a62
Fabian Winkelmann <fabian@fabianwinkelmann.xyz>
2024-11-15 09:27:54 +0100

    generic golang: change go package

--- golang/generic/flake.nix
+++ golang/generic/flake.nix
@@ -13,7 +13,7 @@
   in {
     devShells.x86_64-linux.default = with pkgs;
       mkShellNoCC {
-        packages = with pkgs; [go_1_23 gopls gotools golangci-lint golangci-lint-langserver];
+        packages = with pkgs; [go gopls gotools golangci-lint golangci-lint-langserver];
         shellHook = ''echo "Welcome to the generic Go development environment"'';
       };
   };
