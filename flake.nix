675386ef3b56be2cfe13e83dc203575e7c9eeffb
Fabian Winkelmann <fabian@fabianwinkelmann.xyz>
2024-12-05 15:15:34 +0100

    add flake-compat

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake templates for various development environments";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
   };
 
   outputs = {
--- templates/java/generic/flake.nix
+++ templates/java/generic/flake.nix
@@ -3,11 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
   outputs = {
     self,
     nixpkgs,
+    ...
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
