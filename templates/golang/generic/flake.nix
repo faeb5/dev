e41374185997fecf6d813bd9a04d13bf0fea2c9e
Fabian Winkelmann <fabian@fabianwinkelmann.xyz>
2024-12-05 14:58:36 +0100

    change nixpkgs in templates to 24.11

--- templates/golang/generic/flake.nix
+++ templates/golang/generic/flake.nix
@@ -2,7 +2,7 @@
   description = "Generic Go development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
   };
 
   outputs = {
